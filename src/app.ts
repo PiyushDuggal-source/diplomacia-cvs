68ded5812170745b5f540d2d2995aa4c3017fe60
Raj Jaiswal <rajj91783@gmail.com>
2024-06-27 17:37:10 +0530

    added CORS for www

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(
     store: store,
   }),
 );
-const allowedOrigins = ["http://localhost:3000", "https://iitmdiplomacia.in"];
+const allowedOrigins = ["http://localhost:3000", "https://iitmdiplomacia.in", "https://www.iitmdiplomacia.in"];
 app.use(
   cors({
     credentials: true,
