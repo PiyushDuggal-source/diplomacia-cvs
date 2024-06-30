a20deb1cefdda4e8ea0e27cff5671e8a7a0a0a36
Raj Jaiswal <rajj91783@gmail.com>
2024-06-30 15:30:05 +0530

    fixed spacing and removed CORS fro render

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 export const checkAPI = (req: Request, res: Response, next: NextFunction) => {
   console.log("\nEntering checkAPI");
-  console.log(req.method)
   if (req.headers["x-diplo-api"] !== process.env.DIPLO_API_KEY && req.method != 'GET') {
     res.status(401).send("Unauthorized");
     return;
--- src/routes/pingRoute.ts
+++ src/routes/pingRoute.ts
@@ -2,7 +2,7 @@ import e, { Request, Response } from "express";
 
 const router = e.Router();
 
-router.get("/ping", (req: Request, res: Response)=>{
+router.get("/ping", (req: Request, res: Response) => {
     return res.status(200).json({});
 });
 
